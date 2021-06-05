43ca997b1c6a2a31963e1fd746beb56d1b4c3707
Dario Vogogna <dario.vogogna@vargroup.it>
2021-06-06 00:44:39 +0200

    chore: wip add zsh configuration

---
+++ roles/zsh/aliases.zsh
@@ -0,0 +1,14 @@
+alias hs='history | grep'
+alias cl="clear"
+alias c="clear"
+alias pg='ps -ef | grep'
+alias pkill!="pkill -9 -f "
+
+alias ohmyzsh="vim ~/.oh-my-zsh"
+alias reload!='. ~/.zshrc'
+
+alias vim="nvim"
+alias vi="nvim"
+alias v="nvim"
+
+alias timezsh="time zsh -i -c echo"
