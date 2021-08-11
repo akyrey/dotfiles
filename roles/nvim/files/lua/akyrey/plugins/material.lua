3bf89548650312cc02a1b0d37b83414b0570f410
Dario Vogogna <stein.dario@gmail.com>
2021-08-11 18:37:16 +0200

    Use transparent background with nvim

--- roles/nvim/files/lua/akyrey/plugins/material.lua
+++ roles/nvim/files/lua/akyrey/plugins/material.lua
@@ -4,6 +4,8 @@ local function init()
 
   -- Load colorscheme
   vim.cmd[[colorscheme material]]
+  vim.cmd[[highlight Normal guibg=none]]
+  vim.cmd[[highlight NonText guibg=none]]
   vim.cmd[[hi LineNr ctermfg=0 guifg='#2196f3']]
   vim.cmd[[hi CursorLineNr ctermfg=0 guifg='#FBC02D']]
 end
