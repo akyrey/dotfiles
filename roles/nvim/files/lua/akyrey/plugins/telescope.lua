d6859ca8b6d960a1afdc6f7da5496d4ea8dcef86
Dario Vogogna <stein.dario@gmail.com>
2021-08-11 22:49:58 +0200

    Add telescope settings

--- roles/nvim/files/lua/akyrey/plugins/telescope.lua
+++ roles/nvim/files/lua/akyrey/plugins/telescope.lua
@@ -4,8 +4,13 @@ local actions = require('telescope.actions')
 local function init()
   telescope.setup {
     defaults = {
+      file_sorter = require("telescope.sorters").get_fzy_sorter,
       prompt_prefix = ' >',
       color_devicons = true,
+
+      file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+      grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+      qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
 
       file_ignore_patterns = {
         "node_modules/.*",
