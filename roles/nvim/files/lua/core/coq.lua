9a49bc1fd8338a62f5b1d041bab48a6501e58e03
Dario Vogogna <stein.dario@gmail.com>
2021-09-15 08:38:04 +0200

    Customize coq apperance and delay completions

--- roles/nvim/files/lua/core/coq.lua
+++ roles/nvim/files/lua/core/coq.lua
@@ -6,8 +6,63 @@ M.config = function ()
     clients = {
       tabnine = {
         enabled = true,
-      }
-    }
+        short_name = ' ♖  (T9)',
+        weight_adjust = -2,
+      },
+      buffers = {
+        short_name = '   (Buffer)',
+      },
+      tree_sitter = {
+        short_name = '   (TS)',
+      },
+      paths = {
+        short_name = '   (Path)',
+      },
+      snippets = {
+        short_name = '   (Snippet)',
+      },
+      lsp = {
+        short_name = '   (LSP)',
+        weight_adjust = 2,
+      },
+    },
+    limits = {
+      completion_auto_timeout = 0.2
+    },
+    display = {
+      pum = {
+        source_context = { '', '' },
+      },
+      icons = {
+        mappings = {
+          Text = "",
+          Method = "",
+          Function = "",
+          Constructor = "",
+          Field = "ﰠ",
+          Variable = "",
+          Class = "ﴯ",
+          Interface = "",
+          Module = "",
+          Property = "ﰠ",
+          Unit = "塞",
+          Value = "",
+          Enum = "",
+          Keyword = "",
+          Snippet = "",
+          Color = "",
+          File = "",
+          Reference = "",
+          Folder = "",
+          EnumMember = "",
+          Constant = "",
+          Struct = "פּ",
+          Event = "",
+          Operator = "",
+          TypeParameter = ""
+        },
+      },
+    },
   }
 end
 
