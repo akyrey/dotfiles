471d02812f0e48a9ace47befb36964a0255e9524
Dario Vogogna <stein.dario@gmail.com>
2021-09-26 21:43:39 +0200

    Correctly initialize cmp_nvim_lsp plugin

--- roles/nvim/files/lua/lsp/init.lua
+++ roles/nvim/files/lua/lsp/init.lua
@@ -92,6 +92,10 @@ end
 
 M.common_capabilities = function()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
+  local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+  if status_ok then
+    capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  end
   capabilities.textDocument.completion.completionItem.snippetSupport = true
   capabilities.textDocument.completion.completionItem.resolveSupport = {
     properties = {
