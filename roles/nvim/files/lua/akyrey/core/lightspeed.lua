4061cc206cacd52e80e4ac18e297aec53df799db
Dario Vogogna <stein.dario@gmail.com>
2022-01-13 15:49:39 +0100

    Update lightspeed config fixing breaking changes

--- roles/nvim/files/lua/akyrey/core/lightspeed.lua
+++ roles/nvim/files/lua/akyrey/core/lightspeed.lua
@@ -9,17 +9,13 @@ function M.config()
       exit_after_idle_msecs = { labeled = nil, unlabeled = 1000 },
 
       -- s/x
-      grey_out_search_area = true,
-      highlight_unique_chars = true,
+      jump_to_unique_chars = { safety_timeout = 400 },
       match_only_the_start_of_same_char_seqs = true,
-      jump_on_partial_input_safety_timeout = 400,
       substitute_chars = { ['\r'] = '¬' },
       -- Leaving the appropriate list empty effectively disables
       -- "smart" mode, and forces auto-jump to be on or off.
       -- safe_labels = { ... },
       -- labels = { ... },
-      cycle_group_fwd_key = '<space>',
-      cycle_group_bwd_key = '<tab>',
 
       -- f/t
       limit_ft_matches = 4,
