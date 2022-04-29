0e8bc8f7484d6858141a31ada7b0fd67d541cd04
Dario Vogogna <stein.dario@gmail.com>
2022-04-29 22:19:30 +0200

    Updat nvim bootstrap and impatient profile

--- roles/nvim/files/lua/akyrey/bootstrap.lua
+++ roles/nvim/files/lua/akyrey/bootstrap.lua
@@ -2,6 +2,7 @@ local M = {}
 
 local uv = vim.loop
 local path_sep = uv.os_uname().version:match "Windows" and "\\" or "/"
+local in_headless = #vim.api.nvim_list_uis() == 0
 
 ---Join path segments that were passed as input
 ---@return string
@@ -10,13 +11,23 @@ function _G.join_paths(...)
   return result
 end
 
+---Require a module in protected mode without relying on its cached value
+---@param module string
+---@return any
+function _G.require_clean(module)
+  package.loaded[module] = nil
+  _G[module] = nil
+  local _, requested = pcall(require, module)
+  return requested
+end
+
 ---Get the full path to `$akyreyVIM_RUNTIME_DIR`
 ---@return string
 function _G.get_runtime_dir()
   local akyrey_runtime_dir = os.getenv "AKYREYVIM_RUNTIME_DIR"
   if not akyrey_runtime_dir then
     -- when nvim is used directly
-    return vim.fn.stdpath "data"
+    return vim.call("stdpath", "data")
   end
   return akyrey_runtime_dir
 end
@@ -26,7 +37,7 @@ end
 function _G.get_config_dir()
   local akyrey_config_dir = os.getenv "AKYREYVIM_CONFIG_DIR"
   if not akyrey_config_dir then
-    return vim.fn.stdpath "config"
+    return vim.call("stdpath", "config")
   end
   return akyrey_config_dir
 end
@@ -36,7 +47,7 @@ end
 function _G.get_cache_dir()
   local akyrey_cache_dir = os.getenv "AKYREYVIM_CACHE_DIR"
   if not akyrey_cache_dir then
-    return vim.fn.stdpath "cache"
+    return vim.call("stdpath", "cache")
   end
   return akyrey_cache_dir
 end
@@ -51,6 +62,15 @@ function M:init(base_dir)
   self.packer_install_dir = join_paths(self.runtime_dir, "site", "pack", "packer", "start", "packer.nvim")
   self.packer_cache_path = join_paths(self.config_dir, "plugin", "packer_compiled.lua")
 
+  ---@meta overridden to use LUNARVIM_CACHE_DIR instead, since a lot of plugins call this function interally
+  ---NOTE: changes to "data" are currently unstable, see #2507
+  vim.fn.stdpath = function(what)
+    if what == "cache" then
+      return _G.get_cache_dir()
+    end
+    return vim.call("stdpath", what)
+  end
+
   ---Get the full path to akyreyVim's base directory
   ---@return string
   function _G.get_akyrey_base_dir()
@@ -59,30 +79,24 @@ function M:init(base_dir)
 
   if os.getenv "AKYREYVIM_RUNTIME_DIR" then
     -- vim.opt.rtp:append(os.getenv "AKYREYVIM_RUNTIME_DIR" .. path_sep .. "akyrey")
-    vim.opt.rtp:remove(join_paths(vim.fn.stdpath "data", "site"))
-    vim.opt.rtp:remove(join_paths(vim.fn.stdpath "data", "site", "after"))
+    vim.opt.rtp:remove(join_paths(vim.call("stdpath", "data"), "site"))
+    vim.opt.rtp:remove(join_paths(vim.call("stdpath", "data"), "site", "after"))
     vim.opt.rtp:prepend(join_paths(self.runtime_dir, "site"))
     vim.opt.rtp:append(join_paths(self.runtime_dir, "site", "after"))
 
-    vim.opt.rtp:remove(vim.fn.stdpath "config")
-    vim.opt.rtp:remove(join_paths(vim.fn.stdpath "config", "after"))
+    vim.opt.rtp:remove(vim.call("stdpath", "config"))
+    vim.opt.rtp:remove(join_paths(vim.call("stdpath", "config"), "after"))
     vim.opt.rtp:prepend(self.config_dir)
     vim.opt.rtp:append(join_paths(self.config_dir, "after"))
     -- TODO: we need something like this: vim.opt.packpath = vim.opt.rtp
 
     vim.cmd [[let &packpath = &runtimepath]]
-    vim.cmd("set spellfile=" .. join_paths(self.config_dir, "spell", "en.utf-8.add"))
   end
 
-  vim.fn.mkdir(get_cache_dir(), "p")
-
   -- FIXME: currently unreliable in unit-tests
-  if not os.getenv "AKYREY_TEST_ENV" then
+  if not in_headless then
     _G.PLENARY_DEBUG = false
-    require("akyrey.impatient").setup {
-      path = vim.fn.stdpath "cache" .. "/akyrey_cache",
-      enable_profiling = true,
-    }
+    require "akyrey.impatient"
   end
 
   require("akyrey.config"):init()
@@ -93,35 +107,6 @@ function M:init(base_dir)
   }
 
   return self
-end
-
-local function git_cmd(subcmd, opts)
-  local Job = require "plenary.job"
-  local Log = require "akyrey.core.log"
-  local args = { "-C", opts.cwd }
-  vim.list_extend(args, subcmd)
-
-  local stderr = {}
-  local stdout, ret = Job
-    :new({
-      command = "git",
-      args = args,
-      cwd = opts.cwd,
-      on_stderr = function(_, data)
-        table.insert(stderr, data)
-      end,
-    })
-    :sync()
-
-  if not vim.tbl_isempty(stderr) then
-    Log:debug(stderr)
-  end
-
-  if not vim.tbl_isempty(stdout) then
-    Log:debug(stdout)
-  end
-
-  return ret, stdout
 end
 
 return M
