28b634915ceff8c2df1d72c2a20552046e127d97
Dario Vogogna <dario.vogogna@euronovate.com>
2022-06-07 08:48:40 +0200

    Update zsh aliases

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -4,7 +4,7 @@ alias c="clear"
 alias pg='ps -ef | grep'
 alias pkill!="pkill -9 -f "
 
-alias ohmyzsh="vim ~/.oh-my-zsh"
+alias ohmyzsh="nvim ~/.oh-my-zsh"
 alias reload!='. ~/.zshrc'
 
 alias vim="nvim"
@@ -16,4 +16,8 @@ alias timezsh="time zsh -i -c echo"
 alias dk="docker"
 alias dkc="docker compose"
 
+# Docker login to ECR repo
 alias aws-login="aws ecr get-login-password --region eu-south-1 | docker login --username AWS --password-stdin 422393141836.dkr.ecr.eu-south-1.amazonaws.com"
+
+# Install globally required packages
+alias npm-global="npm i -g eslint eslint_d prettier @fsouza/prettierd npx"
