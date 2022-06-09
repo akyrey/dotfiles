cc2063a1d7a03c1b114570ca4ac7b8a4e0e5969b
Dario Vogogna <dario.vogogna@euronovate.com>
2022-06-09 23:27:26 +0200

    Fix nvim autopairs placing duplicated parentheses

--- roles/nvim/files/lua/akyrey/core/autopairs.lua
+++ roles/nvim/files/lua/akyrey/core/autopairs.lua
@@ -9,12 +9,39 @@ function M.config()
       all = "(",
       tex = "{",
     },
+    ---@usage check bracket in same line
+    enable_check_bracket_line = false,
     ---@usage check treesitter
     check_ts = true,
     ts_config = {
-      lua = { "string" },
-      javascript = { "template_string" },
+      lua = { "string", "source" },
+      javascript = { "string", "template_string" },
       java = false,
+    },
+    disable_filetype = { "TelescopePrompt", "spectre_panel" },
+    ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
+    enable_moveright = true,
+    ---@usage disable when recording or executing a macro
+    disable_in_macro = false,
+    ---@usage add bracket pairs after quote
+    enable_afterquote = true,
+    ---@usage map the <BS> key
+    map_bs = true,
+    ---@usage map <c-w> to delete a pair if possible
+    map_c_w = false,
+    ---@usage disable when insert after visual block mode
+    disable_in_visualblock = false,
+    ---@usage  change default fast_wrap
+    fast_wrap = {
+      map = "<M-e>",
+      chars = { "{", "[", "(", '"', "'" },
+      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+      offset = 0, -- Offset from pattern match
+      end_key = "$",
+      keys = "qwertyuiopzxcvbnmasdfghjkl",
+      check_comma = true,
+      highlight = "Search",
+      highlight_grey = "Comment",
     },
   }
 end
@@ -22,41 +49,21 @@ end
 M.setup = function()
   local autopairs = require "nvim-autopairs"
   local Rule = require "nvim-autopairs.rule"
-  local cond = require "nvim-autopairs.conds"
 
   autopairs.setup {
     check_ts = akyrey.builtin.autopairs.check_ts,
+    enable_check_bracket_line = akyrey.builtin.autopairs.enable_check_bracket_line,
     ts_config = akyrey.builtin.autopairs.ts_config,
+    disable_filetype = akyrey.builtin.autopairs.disable_filetype,
+    disable_in_macro = akyrey.builtin.autopairs.disable_in_macro,
+    ignored_next_char = akyrey.builtin.autopairs.ignored_next_char,
+    enable_moveright = akyrey.builtin.autopairs.enable_moveright,
+    enable_afterquote = akyrey.builtin.autopairs.enable_afterquote,
+    map_c_w = akyrey.builtin.autopairs.map_c_w,
+    map_bs = akyrey.builtin.autopairs.map_bs,
+    disable_in_visualblock = akyrey.builtin.autopairs.disable_in_visualblock,
+    fast_wrap = akyrey.builtin.autopairs.fast_wrap,
   }
-
-  -- vim.g.completion_confirm_key = ""
-
-  autopairs.add_rule(Rule("$$", "$$", "tex"))
-  autopairs.add_rules {
-    Rule("$", "$", { "tex", "latex" }) -- don't add a pair if the next character is %
-      :with_pair(cond.not_after_regex_check "%%") -- don't add a pair if  the previous character is xxx
-      :with_pair(cond.not_before_regex_check("xxx", 3)) -- don't move right when repeat character
-      :with_move(cond.none()) -- don't delete if the next character is xx
-      :with_del(cond.not_after_regex_check "xx") -- disable  add newline when press <cr>
-      :with_cr(cond.none()),
-  }
-  autopairs.add_rules {
-    Rule("$$", "$$", "tex"):with_pair(function(opts)
-      print(vim.inspect(opts))
-      if opts.line == "aa $$" then
-        -- don't add pair on that line
-        return false
-      end
-    end),
-  }
-
-  local cmp_status_ok, cmp = pcall(require, "cmp")
-  if cmp_status_ok then
-    -- If you want insert `(` after select function or method item
-    local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-    local map_char = akyrey.builtin.autopairs.map_char
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = map_char })
-  end
 
   require("nvim-treesitter.configs").setup { autopairs = { enable = true } }
 
