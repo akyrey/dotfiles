7c68f4c380757a5159ff799b743e64f232c777ff
Dario Vogogna <dario.vogogna@euronovate.com>
2022-06-19 16:56:59 +0200

    Use kwallet to store git credentials
    Add environment variable to store git credentials in kwallet

--- roles/zsh/env.zsh
+++ roles/zsh/env.zsh
@@ -3,4 +3,6 @@ export TERM=xterm-256color
 export EDITOR="/usr/local/bin/nvim"
 export VISUAL="/usr/local/bin/nvim"
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+# Use the KDE Wallet to store Git credentials
+export GIT_ASKPASS='/usr/bin/ksshaskpass'
 export DOCKER_BUILDKIT=1
