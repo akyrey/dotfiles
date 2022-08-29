c2329631e315c3a0973e8a490ddab0dceb800e75
Dario Vogogna <dario.vogogna@euronovate.com>
2022-08-29 07:56:57 +0200

    Add GOPATH to environment variables and path

--- roles/zsh/env.zsh
+++ roles/zsh/env.zsh
@@ -5,4 +5,6 @@ export VISUAL="/usr/local/bin/nvim"
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 # Use the KDE Wallet to store Git credentials
 export GIT_ASKPASS='/usr/bin/ksshaskpass'
+# Path to install go app and libraries
+export GOPATH=$HOME/go
 export DOCKER_BUILDKIT=1
--- roles/zsh/path.zsh
+++ roles/zsh/path.zsh
@@ -1,4 +1,5 @@
 export PATH=${PATH}:$ZSH/bin
+export PATH=$PATH:/$HOME/go/bin
 export PATH=$PATH:/$HOME/.dotfiles/bin
 export PATH=$PATH:/$HOME/.local/bin
 export PATH=$PATH:/$HOME/Library/Python/3.8/bin
