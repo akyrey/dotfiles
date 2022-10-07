13e371f0c5bf69899e1f88a84bd88928b6f1dfe8
Dario Vogogna <dario.vogogna@euronovate.com>
2022-10-07 08:23:34 +0200

    Add new nvim cmp source

--- roles/nvim/files/lua/akyrey/plugins.lua
+++ roles/nvim/files/lua/akyrey/plugins.lua
@@ -50,6 +50,7 @@ return {
       { "saadparwaiz1/cmp_luasnip" },
       { "hrsh7th/cmp-buffer" },
       { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-nvim-lsp-signature-help" },
       { "hrsh7th/cmp-path" },
       { "hrsh7th/cmp-nvim-lua" },
       { "f3fora/cmp-spell" },
