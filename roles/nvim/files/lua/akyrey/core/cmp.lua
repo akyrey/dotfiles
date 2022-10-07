9b366554703f15f2adcdf486a9c8b1cdb5410f85
Dario Vogogna <dario.vogogna@euronovate.com>
2022-10-07 08:23:47 +0200

    Fix nvim cmp

--- roles/nvim/files/lua/akyrey/core/cmp.lua
+++ roles/nvim/files/lua/akyrey/core/cmp.lua
@@ -173,6 +173,11 @@ local vim_item_symbol = {
   Variable = " ",
 }
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 M.config = function()
   local status_cmp_ok, cmp = pcall(require, "cmp")
   if not status_cmp_ok then
@@ -223,54 +228,63 @@ M.config = function()
     mapping = {
       ["<C-k>"] = cmp.mapping.select_prev_item(),
       ["<C-j>"] = cmp.mapping.select_next_item(),
+      ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select }, { "i" }),
+      ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select }, { "i" }),
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      -- TODO: potentially fix emmet nonsense
+      ["<C-u>"] = cmp.mapping.scroll_docs(4),
+      ["<C-y>"] = cmp.mapping {
+        i = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false },
+        c = function(fallback)
+          if cmp.visible() then
+            cmp.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false }
+          else
+            fallback()
+          end
+        end,
+      },
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
-        elseif luasnip.expandable() then
-          luasnip.expand()
-        elseif jumpable() then
+        elseif luasnip.expand_or_locally_jumpable() then
+          luasnip.expand_or_jump()
+        elseif jumpable(1) then
           luasnip.jump(1)
-        elseif check_backspace() then
+        elseif has_words_before() then
+          -- cmp.complete()
           fallback()
-        elseif is_emmet_active() then
-          return vim.fn["cmp#complete"]()
         else
           fallback()
         end
-      end, {
-        "i",
-        "s",
-      }),
+      end, { "i", "s" }),
       ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
-        elseif jumpable(-1) then
+        elseif luasnip.jumpable(-1) then
           luasnip.jump(-1)
         else
           fallback()
         end
-      end, {
-        "i",
-        "s",
-      }),
-
+      end, { "i", "s" }),
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
       ["<CR>"] = cmp.mapping(function(fallback)
-        if cmp.visible() and cmp.confirm(akyrey.builtin.cmp.confirm_opts) then
-          return
+        if cmp.visible() then
+          local confirm_opts = vim.deepcopy(akyrey.builtin.cmp.confirm_opts) -- avoid mutating the original opts below
+          local is_insert_mode = function()
+            return vim.api.nvim_get_mode().mode:sub(1, 1) == "i"
+          end
+          if is_insert_mode() then -- prevent overwriting brackets
+            confirm_opts.behavior = cmp.ConfirmBehavior.Insert
+          end
+          if cmp.confirm(confirm_opts) then
+            return -- success, exit early
+          end
         end
 
-        if jumpable() then
-          if not luasnip.jump(1) then
-            fallback()
-          end
-        else
-          fallback()
+        if jumpable(1) and luasnip.jump(1) then
+          return -- success, exit early
         end
+        fallback() -- if not exited early, always fallback
       end),
     },
 
@@ -282,6 +296,7 @@ M.config = function()
 
     sources = {
       { name = "nvim_lsp" },
+      { name = 'nvim_lsp_signature_help' },
       { name = "buffer" },
       { name = "nvim_lua" },
       { name = "cmp_tabnine" },
