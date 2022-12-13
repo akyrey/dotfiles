fb4aff2d4cd18ed4bdca07dd0a4dfa069cb0a5f8
akyrey <stein.dario@gmail.com>
2022-12-13 10:24:34 +0100

    Fix nvim autocommand pattern

--- roles/nvim/files/ftdetect/web.lua
+++ roles/nvim/files/ftdetect/web.lua
@@ -1,3 +1,3 @@
 vim.cmd [[
- au BufRead,BufNewFile *.ts, *.js, *.jsx, *.scss, *.css, *.less, *.html, *.json set shiftwidth=2 softtabstop=2 tabstop=2
+ au BufRead,BufNewFile *.ts,*.js,*.jsx,*.scss,*.css,*.less,*.html,*.json set shiftwidth=2 softtabstop=2 tabstop=2
 ]]
