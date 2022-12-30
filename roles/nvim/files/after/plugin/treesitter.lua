2d43582a4be05041342e75b2973c54a606342de6
akyrey <stein.dario@gmail.com>
2022-12-30 14:52:32 +0100

    Enable treesitter regex highlight

--- roles/nvim/files/after/plugin/treesitter.lua
+++ roles/nvim/files/after/plugin/treesitter.lua
@@ -35,7 +35,7 @@ configs.setup({
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = { "html" },
         -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
         -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
         -- the name of the parser)
