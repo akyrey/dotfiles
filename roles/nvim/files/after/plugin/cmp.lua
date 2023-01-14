44762a18e789703d7cb26e12ae3c529fde7b6b33
akyrey <stein.dario@gmail.com>
2023-01-14 08:47:41 +0100

    Update nvim cmp snippet setup

--- roles/nvim/files/after/plugin/cmp.lua
+++ roles/nvim/files/after/plugin/cmp.lua
@@ -6,107 +6,99 @@ if not c_ok or not l_ok or not t_ok then
     return
 end
 
+-- Taken from LunarVim config
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
 ---when inside a snippet, seeks to the nearest luasnip field if possible, and checks if it is jumpable
 ---@param dir number 1 for forward, -1 for backward; defaults to 1
 ---@return boolean true if a jumpable luasnip field is found while inside a snippet
 local function jumpable(dir)
-    local win_get_cursor = vim.api.nvim_win_get_cursor
-    local get_current_buf = vim.api.nvim_get_current_buf
+  local win_get_cursor = vim.api.nvim_win_get_cursor
+  local get_current_buf = vim.api.nvim_get_current_buf
 
-    local function inside_snippet()
-        -- for outdated versions of luasnip
-        if not luasnip.session.current_nodes then
-            return false
-        end
-
-        local node = luasnip.session.current_nodes[get_current_buf()]
-        if not node then
-            return false
-        end
-
-        local snip_begin_pos, snip_end_pos = node.parent.snippet.mark:pos_begin_end()
-        local pos = win_get_cursor(0)
-        pos[1] = pos[1] - 1 -- LuaSnip is 0-based not 1-based like nvim for rows
-        return pos[1] >= snip_begin_pos[1] and pos[1] <= snip_end_pos[1]
+  ---sets the current buffer's luasnip to the one nearest the cursor
+  ---@return boolean true if a node is found, false otherwise
+  local function seek_luasnip_cursor_node()
+    -- TODO(kylo252): upstream this
+    -- for outdated versions of luasnip
+    if not luasnip.session.current_nodes then
+      return false
     end
 
-    ---sets the current buffer's luasnip to the one nearest the cursor
-    ---@return boolean true if a node is found, false otherwise
-    local function seek_luasnip_cursor_node()
-        -- for outdated versions of luasnip
-        if not luasnip.session.current_nodes then
-            return false
-        end
+    local node = luasnip.session.current_nodes[get_current_buf()]
+    if not node then
+      return false
+    end
 
-        local pos = win_get_cursor(0)
-        pos[1] = pos[1] - 1
-        local node = luasnip.session.current_nodes[get_current_buf()]
-        if not node then
-            return false
-        end
+    local snippet = node.parent.snippet
+    local exit_node = snippet.insert_nodes[0]
 
-        local snippet = node.parent.snippet
-        local exit_node = snippet.insert_nodes[0]
+    local pos = win_get_cursor(0)
+    pos[1] = pos[1] - 1
 
-        -- exit early if we're past the exit node
-        if exit_node then
-            local exit_pos_end = exit_node.mark:pos_end()
-            if (pos[1] > exit_pos_end[1]) or (pos[1] == exit_pos_end[1] and pos[2] > exit_pos_end[2]) then
-                snippet:remove_from_jumplist()
-                luasnip.session.current_nodes[get_current_buf()] = nil
-
-                return false
-            end
-        end
-
-        node = snippet.inner_first:jump_into(1, true)
-        while node ~= nil and node.next ~= nil and node ~= snippet do
-            local n_next = node.next
-            local next_pos = n_next and n_next.mark:pos_begin()
-            local candidate = n_next ~= snippet and next_pos and (pos[1] < next_pos[1])
-            or (pos[1] == next_pos[1] and pos[2] < next_pos[2])
-
-            -- Past unmarked exit node, exit early
-            if n_next == nil or n_next == snippet.next then
-                snippet:remove_from_jumplist()
-                luasnip.session.current_nodes[get_current_buf()] = nil
-
-                return false
-            end
-
-            if candidate then
-                luasnip.session.current_nodes[get_current_buf()] = node
-                return true
-            end
-
-            local ok
-            ok, node = pcall(node.jump_from, node, 1, true) -- no_move until last stop
-            if not ok then
-                snippet:remove_from_jumplist()
-                luasnip.session.current_nodes[get_current_buf()] = nil
-
-                return false
-            end
-        end
-
-        -- No candidate, but have an exit node
-        if exit_node then
-            -- to jump to the exit node, seek to snippet
-            luasnip.session.current_nodes[get_current_buf()] = snippet
-            return true
-        end
-
-        -- No exit node, exit from snippet
+    -- exit early if we're past the exit node
+    if exit_node then
+      local exit_pos_end = exit_node.mark:pos_end()
+      if (pos[1] > exit_pos_end[1]) or (pos[1] == exit_pos_end[1] and pos[2] > exit_pos_end[2]) then
         snippet:remove_from_jumplist()
         luasnip.session.current_nodes[get_current_buf()] = nil
+
         return false
+      end
     end
 
-    if dir == -1 then
-        return inside_snippet() and luasnip.jumpable(-1)
-    else
-        return inside_snippet() and seek_luasnip_cursor_node() and luasnip.jumpable()
+    node = snippet.inner_first:jump_into(1, true)
+    while node ~= nil and node.next ~= nil and node ~= snippet do
+      local n_next = node.next
+      local next_pos = n_next and n_next.mark:pos_begin()
+      local candidate = n_next ~= snippet and next_pos and (pos[1] < next_pos[1])
+        or (pos[1] == next_pos[1] and pos[2] < next_pos[2])
+
+      -- Past unmarked exit node, exit early
+      if n_next == nil or n_next == snippet.next then
+        snippet:remove_from_jumplist()
+        luasnip.session.current_nodes[get_current_buf()] = nil
+
+        return false
+      end
+
+      if candidate then
+        luasnip.session.current_nodes[get_current_buf()] = node
+        return true
+      end
+
+      local ok
+      ok, node = pcall(node.jump_from, node, 1, true) -- no_move until last stop
+      if not ok then
+        snippet:remove_from_jumplist()
+        luasnip.session.current_nodes[get_current_buf()] = nil
+
+        return false
+      end
     end
+
+    -- No candidate, but have an exit node
+    if exit_node then
+      -- to jump to the exit node, seek to snippet
+      luasnip.session.current_nodes[get_current_buf()] = snippet
+      return true
+    end
+
+    -- No exit node, exit from snippet
+    snippet:remove_from_jumplist()
+    luasnip.session.current_nodes[get_current_buf()] = nil
+    return false
+  end
+
+  if dir == -1 then
+    return luasnip.in_snippet() and luasnip.jumpable(-1)
+  else
+    return luasnip.in_snippet() and seek_luasnip_cursor_node() and luasnip.jumpable(1)
+  end
 end
 
 local vim_item_symbol = {
@@ -136,11 +128,6 @@ local vim_item_symbol = {
     Value = " ",
     Variable = " ",
 }
-
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
 
 tabnine:setup({
     max_lines = 1000,
