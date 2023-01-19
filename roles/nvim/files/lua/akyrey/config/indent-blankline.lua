a9ee31592e3ff2645e1fbde8422fa0a013b53a3c
akyrey <stein.dario@gmail.com>
2023-01-19 08:47:14 +0100

    Use treesitter for scope in nvim plugin

--- roles/nvim/files/lua/akyrey/config/indent-blankline.lua
+++ roles/nvim/files/lua/akyrey/config/indent-blankline.lua
@@ -12,6 +12,7 @@ M.setup = function()
         show_current_context = true,
         show_current_context_start = false,
         use_treesitter = true,
+        use_treesitter_scope = true,
     })
 end
 
