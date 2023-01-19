c6fbfc6ca339a0dfff350bb49db83aa7b365008c
akyrey <stein.dario@gmail.com>
2023-01-19 08:47:37 +0100

    Add db management to nvim

---
+++ roles/nvim/files/lua/akyrey/plugins/sql.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "tpope/vim-dadbod",
+        cmd = "DB",
+        dependencies = {
+            "vim-dadbod-completion",
+            "vim-dadbod-ui",
+        },
+        lazy = true,
+    },
+    { "kristijanhusak/vim-dadbod-completion", lazy = true },
+    { "kristijanhusak/vim-dadbod-ui", lazy = true },
+    {
+        "lifepillar/pgsql.vim",
+        config = function()
+            vim.g.sql_type_default = "pgsql"
+        end,
+        ft = "sql",
+        lazy = true,
+    },
+}
