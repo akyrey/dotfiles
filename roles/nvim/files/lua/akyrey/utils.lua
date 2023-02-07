ba4ca0222695679a776feb96fb57e7aa7d378373
akyrey <stein.dario@gmail.com>
2023-02-07 22:27:33 +0100

    Update nvim plugins

--- roles/nvim/files/lua/akyrey/utils.lua
+++ roles/nvim/files/lua/akyrey/utils.lua
@@ -6,20 +6,20 @@ local path_sep = vim.loop.os_uname().version:match "Windows" and "\\" or "/"
 -- @param field (string) field to search for
 -- @returns (bool)
 function M.is_in_package_json(field)
-  if vim.fn.filereadable(vim.fn.getcwd() .. "/package.json") ~= 0 then
-    local package_json = vim.fn.json_decode(vim.fn.readfile "package.json")
-    if package_json[field] ~= nil then
-      return true
+    if vim.fn.filereadable(vim.fn.getcwd() .. "/package.json") ~= 0 then
+        local package_json = vim.fn.json_decode(vim.fn.readfile "package.json")
+        if package_json[field] ~= nil then
+            return true
+        end
+        local dev_dependencies = package_json["devDependencies"]
+        if dev_dependencies ~= nil and dev_dependencies[field] ~= nil then
+            return true
+        end
+        local dependencies = package_json["dependencies"]
+        if dependencies ~= nil and dependencies[field] ~= nil then
+            return true
+        end
     end
-    local dev_dependencies = package_json["devDependencies"]
-    if dev_dependencies ~= nil and dev_dependencies[field] ~= nil then
-      return true
-    end
-    local dependencies = package_json["dependencies"]
-    if dependencies ~= nil and dependencies[field] ~= nil then
-      return true
-    end
-  end
     return false
 end
 
