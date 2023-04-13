ffa11b80e66bcbeb4d501b0b11fd56db40d53bc0
akyrey <stein.dario@gmail.com>
2023-04-13 09:18:06 +0200

    Fix nvim mason plugins update

--- roles/nvim/files/lua/akyrey/config/mason.lua
+++ roles/nvim/files/lua/akyrey/config/mason.lua
@@ -48,7 +48,7 @@ M.setup = function()
     })
 
     mason_null_ls.setup({
-        ensure_installed = nil,
+        ensure_installed = {},
         automatic_installation = true,
         automatic_setup = true,
     })
@@ -60,17 +60,6 @@ M.setup = function()
         }
     })
 
-    mason_null_ls.setup_handlers({
-        function(source_name, methods)
-            -- all sources with no handler get passed here
-            -- To keep the original functionality of `automatic_setup = true`,
-            require("mason-null-ls.automatic_setup")(source_name, methods)
-        end,
-        -- stylua = function(source_name, methods)
-        --   null_ls.register(null_ls.builtins.formatting.stylua)
-        -- end,
-    })
-
     mason_dap.setup({
         ensure_installed = {
             "chrome",
@@ -78,14 +67,6 @@ M.setup = function()
         },
         automatic_installation = true,
         automatic_setup = true,
-    })
-
-    mason_dap.setup_handlers({
-        function(source_name)
-            -- all sources with no handler get passed here
-            -- Keep original functionality of `automatic_setup = true`
-            require('mason-nvim-dap.automatic_setup')(source_name)
-        end,
     })
 
     fidget.setup({
