b1770c4f2f938faf5fdcdbc8a49b5111cc6f1c4c
akyrey <stein.dario@gmail.com>
2023-04-20 08:33:39 +0200

    Update env variables

--- roles/zsh/env.zsh
+++ roles/zsh/env.zsh
@@ -3,15 +3,16 @@ export TERM=xterm-256color
 export EDITOR="/usr/local/bin/nvim"
 export VISUAL="/usr/local/bin/nvim"
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+export XDG_CONFIG_HOME=$HOME/.config
 # Use the KDE Wallet to store Git credentials
 export GIT_ASKPASS='/usr/bin/ksshaskpass'
 # Path to install go app and libraries
 export GOPATH=$HOME/go
 export DOCKER_BUILDKIT=1
 # wSign
-export WSIGN_FRONTEND_DEV_PATH=~/work/wSign/frontend/
-export WSIGN_NODE_DEV_PATH=~/work/wSign/backend/
-export WSIGN_DOCKER_DEV_PATH=~/work/wSign/docker/
+export WSIGN_FRONTEND_DEV_PATH=~/work/wsign/frontend/
+export WSIGN_NODE_DEV_PATH=~/work/wsign/backend/
+export WSIGN_DOCKER_DEV_PATH=~/work/wsign/docker/
 # wID
 export WID_FRONTEND_DEV_PATH=~/work/wid/frontend/
 export WID_BACKEND_DEV_PATH=~/work/wid/backend/
