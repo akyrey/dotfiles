1d06d81f7f945ec9df1dd3376947cb613b169f76
akyrey <stein.dario@gmail.com>
2023-07-22 21:30:17 +0200

    Remove nvim bufferline plugin and update dressing config

--- roles/nvim/files/lua/akyrey/plugins/themes.lua
+++ roles/nvim/files/lua/akyrey/plugins/themes.lua
@@ -21,13 +21,21 @@ return {
         },
     },
     {
-        "akinsho/bufferline.nvim",
-        branch = "main",
-        config = function()
-            require("akyrey.config.bufferline").setup()
+        "stevearc/dressing.nvim",
+        lazy = true,
+        init = function()
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vim.ui.select = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.select(...)
+            end
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vim.ui.input = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.input(...)
+            end
         end,
     },
-    "stevearc/dressing.nvim",
     -- Color highlighter
     {
         "norcalli/nvim-colorizer.lua",
