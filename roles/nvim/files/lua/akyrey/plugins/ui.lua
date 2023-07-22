aa1bd9248bd5b46f792f393e953dfbca28eff0e3
akyrey <stein.dario@gmail.com>
2023-07-22 21:36:05 +0200

    Add nvim-notify plugin

--- roles/nvim/files/lua/akyrey/plugins/ui.lua
+++ roles/nvim/files/lua/akyrey/plugins/ui.lua
@@ -47,4 +47,33 @@ return {
         end,
         event = "BufRead",
     },
+    {
+        "rcarriga/nvim-notify",
+        keys = {
+            {
+                "<leader>un",
+                function()
+                    require("notify").dismiss({ silent = true, pending = true })
+                end,
+                desc = "Dismiss all Notifications",
+            },
+        },
+        init = function()
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "VeryLazy",
+                callback = function()
+                    vim.notify = require("notify")
+                end,
+            })
+        end,
+        opts = {
+            timeout = 3000,
+            max_height = function()
+                return math.floor(vim.o.lines * 0.75)
+            end,
+            max_width = function()
+                return math.floor(vim.o.columns * 0.75)
+            end,
+        },
+    },
 }
