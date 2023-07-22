d45e13b90c46998ff4b1bfe3a8b720a0789a89de
akyrey <stein.dario@gmail.com>
2023-07-22 22:14:24 +0200

    Use nvim schemastore for yaml files

--- roles/nvim/files/lua/akyrey/lsp/config.lua
+++ roles/nvim/files/lua/akyrey/lsp/config.lua
@@ -89,28 +89,13 @@ M.servers = {
         },
     },
     yamlls = {
-        hover = true,
         completion = true,
-        validate = true,
+        hover = true,
         schemaStore = {
-            enable = true,
-            url = "https://www.schemastore.org/api/json/catalog.json",
+            enable = false,
         },
-        schemas = {
-            kubernetes = {
-                "daemon.{yml,yaml}",
-                "manager.{yml,yaml}",
-                "restapi.{yml,yaml}",
-                "role.{yml,yaml}",
-                "role_binding.{yml,yaml}",
-                "*onfigma*.{yml,yaml}",
-                "*ngres*.{yml,yaml}",
-                "*ecre*.{yml,yaml}",
-                "*eployment*.{yml,yaml}",
-                "*ervic*.{yml,yaml}",
-                "kubectl-edit*.yaml",
-            },
-        },
+        schemas = require("schemastore").yaml.schemas(),
+        validate = true,
     },
 }
 
