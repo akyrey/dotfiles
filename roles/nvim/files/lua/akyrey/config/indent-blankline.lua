07e794cb400463212b8d88324e4f753ded0862d3
Dario Vogogna <dario.vogogna@euronovate.com>
2023-10-14 14:41:38 +0200

    Update indent blankline configuration for version 3

--- roles/nvim/files/lua/akyrey/config/indent-blankline.lua
+++ roles/nvim/files/lua/akyrey/config/indent-blankline.lua
@@ -8,24 +8,24 @@ M.setup = function()
     end
 
     indent_blankline.setup({
-        char = "│",
-        filetype_exclude = {
-            "help",
-            "alpha",
-            "dashboard",
-            "neo-tree",
-            "Trouble",
-            "lazy",
-            "mason",
-            "notify",
-            "toggleterm",
-            "lazyterm",
+        exclude = {
+            filetypes = {
+                "help",
+                "alpha",
+                "dashboard",
+                "neo-tree",
+                "Trouble",
+                "lazy",
+                "mason",
+                "notify",
+                "toggleterm",
+                "lazyterm",
+            },
         },
-        space_char_blankline = " ",
-        show_current_context = true,
-        show_current_context_start = false,
-        use_treesitter = true,
-        use_treesitter_scope = true,
+        scope = {
+            show_end = false,
+            show_start = false,
+        },
     })
 end
 
