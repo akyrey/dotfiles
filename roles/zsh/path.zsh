fb58dad7d1f759a3e9df61fad52af905a4cff30e
akyrey <stein.dario@gmail.com>
2023-12-07 22:25:40 +0100

    Update config based on godzilla

--- roles/zsh/path.zsh
+++ roles/zsh/path.zsh
@@ -1,6 +1,9 @@
+export FLYCTL_INSTALL="/home/akyrey/.fly"
+
 export PATH=${PATH}:$ZSH/bin
 export PATH=$PATH:/$HOME/go/bin
 export PATH=$PATH:/$HOME/.dotfiles/bin
 export PATH=$PATH:/$HOME/.local/bin
 export PATH=$PATH:/$HOME/Library/Python/3.8/bin
 export PATH=$PATH:/$HOME/.cargo/bin
+export PATH="$FLYCTL_INSTALL/bin:$PATH"
