f1483a494ffe2c7ec9b4b336b2b426a542dc4d8d
akyrey <stein.dario@gmail.com>
2024-01-06 17:49:32 +0100

    Switch null-ls with none-ls
    none-ls is a community maintained replacement

--- roles/nvim/files/lua/akyrey/plugins/lsp.lua
+++ roles/nvim/files/lua/akyrey/plugins/lsp.lua
@@ -312,7 +312,7 @@ return {
         },
     },
     {
-        "jose-elias-alvarez/null-ls.nvim",
+        "nvimtools/none-ls.nvim",
         dependencies = {
             "williamboman/mason.nvim",
             "nvim-lua/plenary.nvim",
