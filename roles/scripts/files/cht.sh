5eb5871d499f647780faeb580c88d91987842c22
akyrey <stein.dario@gmail.com>
2024-01-16 22:15:28 +0100

    Fix cht.sh script

--- roles/scripts/files/cht.sh
+++ roles/scripts/files/cht.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-languages=`echo "lua typescript nodejs" | tr ' ' '\n'`
+languages=`echo "lua typescript nodejs php" | tr ' ' '\n'`
 core_utils=`echo "xargs find mv sed awk ls zip" | tr ' ' '\n'`
 
 selected=`printf "$languages\n$core_utils" | fzf`
-read -p "query: " query
+if [[ -z $selected ]]; then
+    exit 0
+fi
 
-if printf $languages | grep -qs $selected; then
-  tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
+read -p "Enter query: " query
+
+if printf "$languages" | grep -qs "$selected"; then
+    query=`echo $query | tr ' ' '+'`
+    tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
 else
-  tmux neww bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
 fi
