f5f5f05ad9897a73c27d5cd1b9851daba2f39f2a
akyrey <stein.dario@gmail.com>
2024-01-29 09:40:32 +0100

    Add paths

--- roles/zsh/path.zsh
+++ roles/zsh/path.zsh
@@ -2,8 +2,10 @@ export FLYCTL_INSTALL="/home/akyrey/.fly"
 
 export PATH=${PATH}:$ZSH/bin
 export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$HOME/go/bin
 export PATH=$PATH:/$HOME/.dotfiles/bin
 export PATH=$PATH:/$HOME/.local/bin
 export PATH=$PATH:/$HOME/Library/Python/3.8/bin
 export PATH=$PATH:/$HOME/.cargo/bin
+export PATH=$PATH:/opt/Postman/app
 export PATH="$FLYCTL_INSTALL/bin:$PATH"
