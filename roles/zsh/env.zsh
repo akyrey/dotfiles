a528e2bae7cb41f2890ce38bb46dcec00791f0a8
akyrey <stein.dario@gmail.com>
2024-01-29 09:43:30 +0100

    Revert "Change git credential helper"
    This reverts commit 0fd2a44b06065b8f2d4b440717a264deb080d4df.

--- roles/zsh/env.zsh
+++ roles/zsh/env.zsh
@@ -5,6 +5,8 @@ export EDITOR="/usr/local/bin/nvim"
 export VISUAL="/usr/local/bin/nvim"
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 export XDG_CONFIG_HOME=$HOME/.config
+# Use the KDE Wallet to store Git credentials
+export GIT_ASKPASS='/usr/bin/ksshaskpass'
 # Path to install go app and libraries
 export GOPATH=$HOME/go
 export DOCKER_BUILDKIT=1
