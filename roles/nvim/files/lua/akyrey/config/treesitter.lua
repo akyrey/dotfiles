791a537a6198a671716e55fa6ff80240e048aa69
akyrey <stein.dario@gmail.com>
2024-02-02 08:46:20 +0100

    Add treesitter plugin for templ files

--- roles/nvim/files/lua/akyrey/config/treesitter.lua
+++ roles/nvim/files/lua/akyrey/config/treesitter.lua
@@ -42,7 +42,7 @@ M.setup = function()
             -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
             -- Using this option may slow down your editor, and you may see some duplicate highlights.
             -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = { "html" },
+            additional_vim_regex_highlighting = { "markdown" },
             -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
             -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
             -- the name of the parser)
@@ -146,6 +146,14 @@ M.setup = function()
         },
         filetype = "blade"
     }
+    parser_config.templ = {
+        install_info = {
+            url = "https://github.com/vrischmann/tree-sitter-templ.git",
+            files = { "src/parser.c", "src/scanner.c" },
+            branch = "master",
+        },
+    }
+    vim.treesitter.language.register("templ", "templ")
 
     local ok_ts, ts_context_commentstring = pcall(require, "ts_context_commentstring")
 
