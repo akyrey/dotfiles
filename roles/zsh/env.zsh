0956c11dcf9742b8905e1f6a4e641e95dc2acfb1
akyrey <stein.dario@gmail.com>
2024-02-21 22:04:37 +0100

    Update git configurations

--- roles/zsh/env.zsh
+++ roles/zsh/env.zsh
@@ -7,6 +7,7 @@ export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 export XDG_CONFIG_HOME=$HOME/.config
 # Use the KDE Wallet to store Git credentials
 export GIT_ASKPASS='/usr/bin/ksshaskpass'
+export GPG_TTY=$(tty)
 # Path to install go app and libraries
 export GOPATH=$HOME/go
 export DOCKER_BUILDKIT=1
