8b2698cea8e8c47c1e22d395a72c995ca3f96b66
akyrey <stein.dario@gmail.com>
2024-02-23 09:41:54 +0100

    Color lines like the previous version

--- roles/nvim/files/lua/config/autocmds.lua
+++ roles/nvim/files/lua/config/autocmds.lua
@@ -10,3 +10,12 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.wo.conceallevel = 0
   end,
 })
+
+-- Color line numbers
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#2196f3" })
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FBC02D" })
+  end,
+})
--- roles/nvim/files/lua/plugins/colorscheme.lua
+++ roles/nvim/files/lua/plugins/colorscheme.lua
@@ -5,10 +5,6 @@ return {
     opts = function(_, opts)
       opts.flavour = "macchiato"
       opts.transparent_background = true
-
-      -- Color line numbers
-      vim.api.nvim_set_hl(0, "LineNr", { fg = "#2196f3" })
-      vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FBC02D" })
     end,
   },
   {
