41e7f2c804bc23b4a3b2af9232e70b61db2ab005
akyrey <stein.dario@gmail.com>
2024-03-01 22:09:37 +0100

    Harpoon marks by branch

--- roles/nvim/files/lua/plugins/harpoon.lua
+++ roles/nvim/files/lua/plugins/harpoon.lua
@@ -5,5 +5,45 @@ return {
     "nvim-lua/plenary.nvim",
   },
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    ---@type HarpoonSettings
+    settings = {
+      save_on_toggle = true,
+      sync_on_ui_close = true,
+      key = function()
+        local Job = require("plenary.job")
+
+        local function get_os_command_output(cmd, cwd)
+          if type(cmd) ~= "table" then
+            return {}
+          end
+          local command = table.remove(cmd, 1)
+          local stderr = {}
+          local stdout, ret = Job:new({
+            command = command,
+            args = cmd,
+            cwd = cwd,
+            on_stderr = function(_, data)
+              table.insert(stderr, data)
+            end,
+          }):sync()
+          return stdout, ret, stderr
+        end
+
+        -- Use git branch name if available
+        local branch = get_os_command_output({
+          "git",
+          "rev-parse",
+          "--abbrev-ref",
+          "HEAD",
+        })[1]
+
+        if branch then
+          return vim.loop.cwd() .. "-" .. branch
+        else
+          return vim.loop.cwd()
+        end
+      end,
+    },
+  },
 }
