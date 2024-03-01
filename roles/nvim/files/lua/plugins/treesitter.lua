f512656db9fdf482ea8433c069a624834481848c
akyrey <stein.dario@gmail.com>
2024-03-01 22:44:34 +0100

    Use nvim-autopairs instead of mini.pairs

--- roles/nvim/files/lua/plugins/treesitter.lua
+++ roles/nvim/files/lua/plugins/treesitter.lua
@@ -1,6 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function()
+  opts = function(_, opts)
+    opts.autopairs = {
+      enable = true,
+    }
+
     local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
     parser_config.blade = {
       install_info = {
