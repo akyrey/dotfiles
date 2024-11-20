ba2e05fd2294993d172fda5bd4f87ec162f8662d
akyrey <stein.dario@gmail.com>
2024-11-21 00:16:56 +0100

    Fix configuration on popos

--- roles/zsh/env.zsh
+++ roles/zsh/env.zsh
@@ -5,8 +5,6 @@ export EDITOR="/usr/local/bin/nvim"
 export VISUAL="/usr/local/bin/nvim"
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 export XDG_CONFIG_HOME=$HOME/.config
-# Use the KDE Wallet to store Git credentials
-export GIT_ASKPASS='/usr/bin/ksshaskpass'
 export GPG_TTY=$(tty)
 # Path to install go app and libraries
 export GOPATH=$HOME/go
