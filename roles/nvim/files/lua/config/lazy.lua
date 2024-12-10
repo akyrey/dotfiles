df9d4c576ef13f06dc5516fa74918037534e062b
akyrey <stein.dario@gmail.com>
2024-12-10 22:47:45 +0100

    Add copilot chat to nvim

--- roles/nvim/files/lua/config/lazy.lua
+++ roles/nvim/files/lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.dap.core" },
