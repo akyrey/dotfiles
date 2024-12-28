81473d5c03e1804cb690697db7e93b529dd433a9
akyrey <stein.dario@gmail.com>
2024-12-28 16:52:08 +0100

    Install laravel.nvim plugin

--- roles/nvim/files/lua/plugins/editor.lua
+++ roles/nvim/files/lua/plugins/editor.lua
@@ -85,4 +85,21 @@ return {
       },
     },
   },
+  {
+    "adalessa/laravel.nvim",
+    dependencies = {
+      "tpope/vim-dotenv",
+      "MunifTanjim/nui.nvim",
+      "kevinhwang91/promise-async",
+    },
+    cmd = { "Laravel" },
+    keys = {
+      { "<leader>la", ":Laravel artisan<cr>" },
+      { "<leader>lr", ":Laravel routes<cr>" },
+      { "<leader>lm", ":Laravel related<cr>" },
+    },
+    event = { "VeryLazy" },
+    opts = {},
+    config = true,
+  },
 }
