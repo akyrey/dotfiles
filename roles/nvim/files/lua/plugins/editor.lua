34f8713005119ae5a6591b22211072a29a42517d
akyrey <stein.dario@gmail.com>
2026-03-09 17:42:58 +0100

    Fix laravel.nvim setup

--- roles/nvim/files/lua/plugins/editor.lua
+++ roles/nvim/files/lua/plugins/editor.lua
@@ -59,6 +59,19 @@ return {
   },
   {
     "saghen/blink.cmp",
+    -- use a release tag to download pre-built binaries
+    version = "1.*",
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+      providers = {
+        laravel = {
+          name = "laravel",
+          module = "blink.compat.source",
+        },
+      },
+    },
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     opts = {
       keymap = {
         preset = "enter",
@@ -83,20 +96,127 @@ return {
     },
   },
   {
+    "saghen/blink.compat",
+    -- use v2.* for blink.cmp v1.*
+    version = "2.*",
+    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    lazy = true,
+    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+    opts = {},
+  },
+  {
     "adalessa/laravel.nvim",
     dependencies = {
-      "tpope/vim-dotenv",
       "MunifTanjim/nui.nvim",
-      "kevinhwang91/promise-async",
+      "nvim-lua/plenary.nvim",
+      "nvim-neotest/nvim-nio",
     },
     cmd = { "Laravel" },
-    keys = {
-      { "<leader>la", ":Laravel artisan<cr>" },
-      { "<leader>lr", ":Laravel routes<cr>" },
-      { "<leader>lm", ":Laravel related<cr>" },
+    ft = { "php", "blade" },
+    event = {
+      "BufEnter composer.json",
     },
-    event = { "VeryLazy" },
-    opts = {},
-    config = true,
+    keys = {
+      {
+        "<leader>ll",
+        function()
+          Laravel.pickers.laravel()
+        end,
+        desc = "Laravel: Open Laravel Picker",
+      },
+      {
+        "<c-g>",
+        function()
+          Laravel.commands.run("view:finder")
+        end,
+        desc = "Laravel: Open View Finder",
+      },
+      {
+        "<leader>la",
+        function()
+          Laravel.pickers.artisan()
+        end,
+        desc = "Laravel: Open Artisan Picker",
+      },
+      {
+        "<leader>lt",
+        function()
+          Laravel.commands.run("actions")
+        end,
+        desc = "Laravel: Open Actions Picker",
+      },
+      {
+        "<leader>lr",
+        function()
+          Laravel.pickers.routes()
+        end,
+        desc = "Laravel: Open Routes Picker",
+      },
+      {
+        "<leader>lh",
+        function()
+          Laravel.run("artisan docs")
+        end,
+        desc = "Laravel: Open Documentation",
+      },
+      {
+        "<leader>lm",
+        function()
+          Laravel.pickers.make()
+        end,
+        desc = "Laravel: Open Make Picker",
+      },
+      {
+        "<leader>lc",
+        function()
+          Laravel.pickers.commands()
+        end,
+        desc = "Laravel: Open Commands Picker",
+      },
+      {
+        "<leader>lo",
+        function()
+          Laravel.pickers.resources()
+        end,
+        desc = "Laravel: Open Resources Picker",
+      },
+      {
+        "<leader>lp",
+        function()
+          Laravel.commands.run("command_center")
+        end,
+        desc = "Laravel: Open Command Center",
+      },
+      {
+        "<leader>lu",
+        function()
+          Laravel.commands.run("hub")
+        end,
+        desc = "Laravel Artisan hub",
+      },
+      {
+        "gf",
+        function()
+          local ok, res = pcall(function()
+            if Laravel.app("gf").cursorOnResource() then
+              return "<cmd>lua Laravel.commands.run('gf')<cr>"
+            end
+          end)
+          if not ok or not res then
+            return "gf"
+          end
+          return res
+        end,
+        expr = true,
+        noremap = true,
+      },
+    },
+    opts = {
+      features = {
+        pickers = {
+          provider = "snacks", -- "snacks | telescope | fzf-lua | ui-select"
+        },
+      },
+    },
   },
 }
