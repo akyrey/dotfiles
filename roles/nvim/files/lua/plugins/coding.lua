f4e5ce02d0cb516b78c36ea7c806394b4dfa95e4
akyrey <stein.dario@gmail.com>
2026-05-05 11:56:21 +0200

    Install nvim plugins to preview openapi docs and condition ordering

--- roles/nvim/files/lua/plugins/coding.lua
+++ roles/nvim/files/lua/plugins/coding.lua
@@ -1,6 +1,23 @@
 return {
-  "zbirenbaum/copilot.lua",
-  opts = {
-    copilot_node_command = vim.fn.expand("$HOME") .. "/.asdf/installs/nodejs/24.14.0/bin/node", -- Node.js version must be > 18.x
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      copilot_node_command = vim.fn.expand("$HOME") .. "/.asdf/installs/nodejs/24.14.0/bin/node", -- Node.js version must be > 18.x
+    },
+  },
+  {
+    "akyrey/condition-order.nvim",
+    ft = { "php", "go" },
+    opts = {},
+  },
+  {
+    "akyrey/openapi-navigator.nvim",
+    ft = { "yaml", "json" },
+    opts = {
+      laravel = {
+        cmd = { "./xenv", "artisan", "route:list", "--json" },
+      },
+    },
+  },
   },
 }
